ffe2d75dc343308395cd471d127cd0265065a85b
Jhordy Camacas <jhordycamacas9@gmail.com>
2024-10-17 11:13:21 -0500

    tercer commit, trabajando con con Git y GithubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author utpl
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo dos básico de Java\n" +
+"Versionado a través de Git\n" +
+"Manejado desde GitHub\n" +
+"Además de usar GitHubClassroom");
+    }
+    
+}
